97a93f9347ffa3e5cdfed37892b18525f8af1389
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-05-25 19:13:02 +0300

    fix: method getHmac

--- src/components/crypto-generator.ts
+++ src/components/crypto-generator.ts
@@ -1,17 +1,11 @@
 const {
-  createHash, createHmac
+  createHmac, randomBytes
 } = await import('node:crypto');
-import { v4 as uuidv4 } from 'uuid';
 
 export class CryptoGenerator {
-  getHash(key: string) {
-    const hash = createHash("SHA3-256");
-    hash.update(key);
-    return hash.digest("hex");
-  }
 
   getHmac(secret: string) {
-    const key = this.getHash(uuidv4());
+    const key = randomBytes(32).toString("hex");
     const hmac = createHmac("SHA3-256", key).update(secret).digest("hex");
     return { key, hmac }
   }
